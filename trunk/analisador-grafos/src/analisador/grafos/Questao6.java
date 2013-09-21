c2ff2cb8ed9be26a0a701cf4e952e3be2a883947
gustavo.sabel.gs@gmail.com <gustavo.sabel.gs@gmail.com@031229d6-08c8-c0ef-197c-fb5115933f0c>
2013-09-21 13:41:20 +0000

    Tratando erros de entrada do usuário.

--- trunk/analisador-grafos/src/analisador/grafos/Questao6.java
+++ trunk/analisador-grafos/src/analisador/grafos/Questao6.java
@@ -1,6 +1,154 @@
 package analisador.grafos;
 
+import java.util.Scanner;
+
 public class Questao6 {
+
+    public static final int Infinito = Integer.MAX_VALUE;
+
+    public void menorCaminho() {
+        Scanner scan = new Scanner(System.in);
+        String valorMatriz;
+        String[] vetorAux;
+        char inicio;
+        char fim;
+        int tamanho;
+        int[][] matriz;
+
+        while (true) {
+            try {
+                tamanho = Integer.parseInt(scan.nextLine());
+                matriz = new int[tamanho][tamanho];
+                break;
+            } catch (NumberFormatException ex) {
+                System.out.println("Valor incorretos. Informe o tamanho do grafo");
+            } catch (NegativeArraySizeException ex) {
+                System.out.println("Valor incorretos. Informe um valor positivo.");
+            }
+        }
+
+        while (true) {
+            try {
+                String iniFim = scan.nextLine();
+                inicio = iniFim.charAt(0);
+                fim = iniFim.charAt(2);
+
+                if (toNumero(inicio) < 0 || toNumero(inicio) > tamanho - 1
+                        || toNumero(fim) < 0 || toNumero(fim) > tamanho - 1) {
+                    throw new IndexOutOfBoundsException();
+                }
+
+                break;
+            } catch (IndexOutOfBoundsException ex) {
+                System.out.println(
+                        "Valores incorretos. Informe o vértice de inicio e fim entre A e " + (char) (64 + tamanho));
+            }
+        }
+
+        for (int i = 0; i < tamanho; i++) {
+            while (true) {
+                try {
+                    valorMatriz = scan.nextLine();
+                    vetorAux = valorMatriz.split(" ");
+
+                    for (int j = 0; j < vetorAux.length; j++) {
+                        if (vetorAux[j].equalsIgnoreCase("I")) {
+                            matriz[i][j] = Infinito;
+                        } else {
+                            matriz[i][j] = Integer.parseInt(vetorAux[j]);
+                        }
+                    }
+                    break;
+                } catch (IndexOutOfBoundsException ex) {
+                    System.out.println(
+                            "Valores incorretos. Deve ser informado " + tamanho + " numeros por Linha. "
+                            + "Poder ser informado I para representar o infinito");
+                } catch (NumberFormatException ex) {
+                    System.out.println(
+                            "Valores incorretos. Deve ser informado " + tamanho + " numeros por Linha. "
+                            + "Poder ser informado I para representar o infinito");
+                }
+            }
+        }
+
+        System.out.println(new Questao6().menorCaminho(matriz, inicio, fim));
+    }
+
+    public String menorCaminho(int[][] grafo, char inicio, char fim) {
+        int inicioInt = toNumero(inicio);
+        int fimInt = toNumero(fim);
+
+        Vertice[] vertices = new Vertice[grafo.length];
+        for (int i = 0; i < vertices.length; i++) {
+            vertices[i] = new Vertice();
+            vertices[i].custo = Infinito;
+            vertices[i].pai = -1;
+        }
+        vertices[inicioInt].custo = 0;
+
+        while (!todosProcessados(vertices)) {
+            int u = retornaMenor(vertices);
+            vertices[u].processado = true;
+
+            for (int i = 0; i < grafo.length; i++) {
+                if (grafo[u][i] < Infinito) {
+                    Relax(u, i, grafo[u][i], vertices);
+                }
+            }
+        }
+
+        String saida = "Caminho mínimo: " + imprime(fimInt, vertices);
+        if (saida.length() > 0) {
+            saida = saida.substring(0, saida.length() - 4);
+        }
+        saida += "\nCusto: " + vertices[fimInt].custo;
+        return saida;
+    }
+
+    private int retornaMenor(Vertice[] vertices) {
+        int menor = -1;
+        int custoMenor = Infinito;
+        for (int i = 0; i < vertices.length; i++) {
+            if (!vertices[i].isProcessado()) {
+                if (vertices[i].custo < custoMenor) {
+                    menor = i;
+                    custoMenor = vertices[i].custo;
+                }
+            }
+        }
+        return menor;
+    }
+
+    private boolean todosProcessados(Vertice[] vertices) {
+        for (int i = 1; i < vertices.length; i++) {
+            if (!vertices[i].isProcessado()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private String imprime(int u, Vertice[] vertices) {
+        if (u < 0) {
+            return "";
+        }
+        return imprime(vertices[u].pai, vertices) + toLetra(u) + " -> ";
+    }
+
+    private void Relax(int u, int v, int w, Vertice[] vertices) {
+        if (vertices[v].custo > vertices[u].custo + w) {
+            vertices[v].custo = vertices[u].custo + w;
+            vertices[v].pai = u;
+        }
+    }
+
+    private int toNumero(char letra) {
+        return letra - 65;
+    }
+
+    private char toLetra(int numero) {
+        return (char) (numero + 65);
+    }
 
     private class Vertice {
 
@@ -30,69 +178,6 @@ public class Questao6 {
 
         public void setProcessado(boolean verificado) {
             this.processado = verificado;
-        }
-    }
-    
-    private int retornaMenor(Vertice[] vertices) {
-        int menor = 0;
-        for (int i = 1; i < vertices.length; i++) {
-            if (!vertices[i].isProcessado()) {
-                if (vertices[menor].custo > vertices[i].custo) {
-                    menor = i;
-                }
-            }
-        }
-        return menor;
-    }
-
-    private boolean todosProcessados(Vertice[] vertices) {
-        for (int i = 1; i < vertices.length; i++) {
-            if (!vertices[i].isProcessado()) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public String menorCaminho(int[][] grafo, int inicio, int fim) {
-        Vertice[] vertices = new Vertice[grafo.length];
-        for (int i = 0; i < vertices.length; i++) {
-            vertices[i] = new Vertice();
-            vertices[i].custo = Integer.MAX_VALUE;
-            vertices[i].pai = -1;
-        }
-        vertices[inicio].custo = 0;
-
-        while (!todosProcessados(vertices)) {
-            int u = retornaMenor(vertices);
-            vertices[u].processado = true;
-
-            for (int i = 0; i < grafo.length; i++) {
-                if (grafo[u][i] < Integer.MAX_VALUE) {
-                    Relax(u, i, grafo[u][i], vertices);
-                }
-            }
-        }
-        
-        String saida = "Caminho mínimo: " + imprime(fim, vertices);
-        if(saida.length() > 0)
-            saida = saida.substring(0, saida.length() - 4 );
-        saida += "\nCusto: " + vertices[fim].custo;
-        return saida;
-    }
-    
-    
-    private String imprime(int u, Vertice[] vertices)
-    {
-        if(u < 0)
-            return "";
-        return imprime(vertices[u].pai, vertices) + (char) (u + 65) + " -> ";
-    }
-
-    private void Relax(int u, int v, int w, Vertice[] vertices) {
-        if (vertices[v].custo > vertices[u].custo + w) {
-            vertices[v].custo = vertices[u].custo + w;
-            vertices[v].pai = u;
         }
     }
 }
